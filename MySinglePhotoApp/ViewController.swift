666e7a0e710e69bb40cfcb98333d561bf34042e8
cs_pple_02 <cs_pple_02@cs-pple-02s-iMac.local>
2019-09-24 12:15:55 -0500

    Tipc Calculator first commit

---
+++ MySinglePhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySinglePhotoApp
+//
+//  Created by cs_pple_02 on 7/15/19.
+//  Copyright © 2019 cs_pple_02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
